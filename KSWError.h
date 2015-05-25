4fed402f7fcf4286da71df747e165c38d3a6b27c
Mike Abdullah <me@mikeabdullah.net>
2015-05-25 17:04:16 +0200

    Start of KSWError project

---
+++ KSWError.h
@@ -0,0 +1,19 @@
+//
+//  KSWError.h
+//  KSWError
+//
+//  Created by Mike on 25/05/2015.
+//  Copyright (c) 2015 Karelia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSWError.
+FOUNDATION_EXPORT double KSWErrorVersionNumber;
+
+//! Project version string for KSWError.
+FOUNDATION_EXPORT const unsigned char KSWErrorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSWError/PublicHeader.h>
+
+
